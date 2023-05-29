127e82c7caab13163d4a1ffffa6ee618c2f92846
PatrikGjikolaj <123626448+PatrikGjikolaj@users.noreply.github.com>
2023-05-29 12:54:37 +0200

    add: vuetify

--- src/components/AppFilm.vue
+++ src/components/AppFilm.vue
@@ -31,24 +31,54 @@ export default {
                     <h2 class="text-danger h50">Films</h2>
                 </div>
             </div>
-            <div class="row flex-nowrap altezza100">
-                <div class="col-2 bg-light border-start border-end p-0" v-for="(film, c) in store.films">
-                    <img :src="store.imgFilm + film.backdrop_path" alt="" class="cover">
-                    <h4>{{ film.title }}</h4>
-                    <h5 class="tronca">{{ film.original_title }}</h5>
-                    <img :src="`https://flagcdn.com/16x12/${film.original_language}.png`" alt="" class="flags">
-                    <h6>Stelle:
-                        <template v-for="i in stelle">
-                            <template v-if="Math.ceil(film.vote_average / 2) >= i">
-                                <span>&#9733;</span>
-                            </template>
-                            <template v-else>
-                                <span>&#9734;</span>
-                            </template>
-                        </template>
-                    </h6>
-                </div>
+            <div>
+                <v-hover v-slot="{ isHovering, props }">
+                    <v-card class="mx-auto" color="grey-lighten-4" max-width="600" v-bind="props">
+                        <v-img :aspect-ratio="16 / 9" cover src="https://cdn.vuetifyjs.com/images/cards/kitchen.png">
+                            <v-expand-transition>
+                                <div v-if="isHovering"
+                                class="d-flex transition-fast-in-fast-out bg-orange-darken-2 v-card--reveal text-h2"
+                                style="height: 100%;">
+                                $14.99
+                            </div>
+                        </v-expand-transition>
+                    </v-img>
+
+                    <v-card-text class="pt-6">
+                        <div class="font-weight-light text-grey text-h6 mb-2">
+                            For the perfect meal
+                        </div>
+
+                        <h3 class="text-h4 font-weight-light text-orange mb-2">
+                            QW cooking utensils
+                        </h3>
+
+                        <div class="font-weight-light text-h6 mb-2">
+                                Our Vintage kitchen utensils delight any chef.<br>
+                                Made of bamboo by hand
+                            </div>
+                        </v-card-text>
+                    </v-card>
+                </v-hover>
             </div>
+            <!-- <div class="row flex-nowrap altezza100">
+                                <div class="col-2 bg-light border-start border-end p-0" v-for="(film, c) in store.films">
+                                    <img :src="store.imgFilm + film.backdrop_path" alt="" class="cover">
+                                    <h4>{{ film.title }}</h4>
+                                    <h5 class="tronca">{{ film.original_title }}</h5>
+                                    <img :src="`https://flagcdn.com/16x12/${film.original_language}.png`" alt="" class="flags">
+                                    <h6>Stelle:
+                                        <template v-for="i in stelle">
+                                            <template v-if="Math.ceil(film.vote_average / 2) >= i">
+                                                <span>&#9733;</span>
+                                            </template>
+                                            <template v-else>
+                                                <span>&#9734;</span>
+                                            </template>
+                                        </template>
+                                    </h6>
+                                </div>
+                            </div> -->
         </div>
     </div>
 </template>
@@ -86,5 +116,14 @@ export default {
     overflow: hidden;
     text-overflow: ellipsis;
     width: 100%
+}
+
+.v-card--reveal {
+    align-items: center;
+    bottom: 0;
+    justify-content: center;
+    opacity: .9;
+    position: absolute;
+    width: 100%;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -4,5 +4,16 @@ import App from './App.vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
 
-createApp(App).mount('#app')
+const vuetify = createVuetify({
+  components,
+  directives,
+})
+
+export default createVuetify()
+
+createApp(App).use(vuetify).mount('#app')
